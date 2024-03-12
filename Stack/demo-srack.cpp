8410fd860957f9af8b7b493d0539b8dd0ca1720e
jeevandas-jd <142812462+jeevandas-jd@users.noreply.github.com>
2024-03-12 22:18:41 +0530

    demo-srack.cpp

--- Stack/demo-srack.cpp
+++ Stack/demo-srack.cpp
@@ -1,21 +1,33 @@
 #include "Stack.cpp"
 
-int main(){
-    Stack<int> s(5);
-    bool c= true;
-    while(c==true){
-        int a;
-        cout<<"enter element\n>>";
-        cin>>a;
-        s.Push(a);
-        cout<<"continue 1/0";
-        cin>>a;
-        if(a==1){
-            c= true;
-        }
-        else{
-            c= false;
-        }
-    }
-    cout<<s;
+int main() {
+    Stack<int> intStack(5);
+
+    intStack.Push(10);
+    intStack.Push(20);
+    intStack.Push(30);
+    intStack.Push(40);
+    intStack.Push(50);
+
+    cout << "Stack after pushing elements:" << endl;
+    cout<<intStack<<endl;
+
+    if (intStack.IsFull())
+        cout << "Stack is full." << endl;
+    else
+        cout << "Stack is not full." << endl;
+
+    cout << "Top element of the stack: " << intStack.Peek() << endl;
+
+    intStack.Pop();
+
+    cout << "Stack after popping an element:" << endl;
+    cout<<intStack<<endl;
+
+    if (intStack.IsEmpty())
+        cout << "Stack is empty." << endl;
+    else
+        cout << "Stack is not empty." << endl;
+
+    return 0;
 }
